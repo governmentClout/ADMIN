3c8fc3ecab4e35b27acaf38b2c7c0ba2414305af
Seyi Onifade <xyluz@ymail.com>
2018-12-06 10:57:33 -0800

    session class backbone

---
+++ components/class/session.class.php
@@ -0,0 +1,34 @@
+<?php 
+
+class Session {
+    
+    protected $isLogged = false;
+    
+    
+    public function __construct() {
+       session_start();       
+               
+    }
+
+    public function checkUser(){
+
+        if(isset($_SESSION['loggedIn'])){
+            $this->isLogged = true;
+        }
+        return $this->isLogged;
+    }
+
+    public function login($id){
+
+        $_SESSION['loggedIn'] = $id;
+
+    }
+
+    public function logout(){
+
+        unset($_SESSION['loggedIn']);
+        session_destroy();
+
+    }
+
+}
